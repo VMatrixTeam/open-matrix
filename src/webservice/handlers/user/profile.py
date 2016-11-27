20a8208d4690a367754849f6c427967b50356544
kungcn <kcnnow@gmail.com>
2016-11-27 11:01:27 +0800

    QAs.number done & manage some question

--- src/webservice/handlers/user/profile.py
+++ src/webservice/handlers/user/profile.py
@@ -17,19 +17,19 @@ class ProfileHandler(BaseController):
 
       user = yield User.get_user_by_id(user_id)
       timeline_recs = yield self.process_timeline_data(user_id)
+      num = yield self.QAs_num(user_id)
+      question = yield self.QAs_ques(user_id)
 
-      self.render("user/profile.jade", profile_user=user, current_user=self.current_user, records=timeline_recs)
+      self.render("user/profile.jade", profile_user=user, current_user=self.current_user, records=timeline_recs, number=num, questions = question)
 
     @gen.coroutine
     def process_timeline_data(self, user_id):
       q_records = yield User.get_question_records_by_user_id(user_id)
       a_records = yield User.get_answer_records_by_user_id(user_id)
       c_record = yield User.get_oldest_comment_record_by_user_id(user_id)
-      print 'q_records: ',q_records
       records = self.add_attrs(q_records, 'Question') + self.add_attrs(a_records, 'Answer') + self.add_attrs(c_record, 'Comment')
 
       sort_recs = self.sort_by_time(records)
-      print 'sort_recs: ', sort_recs
       raise gen.Return(sort_recs)
 
     ##
@@ -79,3 +79,28 @@ class ProfileHandler(BaseController):
           pass
 
       return sort_recs
+
+
+    @gen.coroutine
+    def QAs_num(self, user_id):
+      question = yield User.get_question_records_by_user_id(user_id)
+      answer = yield User.get_answer_records_by_user_id(user_id)
+      #vote = yield User.get_vote_records_by_user_id(user_id)
+      #reputation = yield User.get_reputation_records_by_user_id(user_id)
+
+      number = {'answer' : len(answer), 'question' : len(question), 'vote' : 0, 'reputation' : 0};
+
+      raise gen.Return(number)
+
+    @gen.coroutine
+    def QAs_ques(self, user_id):
+      question = yield User.get_question_records_by_user_id(user_id)
+      result = [0 for i in range(5)]
+
+      if len(question) <= 5:
+        result  = question
+      else:
+        for i in range(len(result)):
+          result[i] = question[i]
+
+      raise gen.Return(result)
